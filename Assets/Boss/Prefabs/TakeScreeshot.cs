9dd66636163b21fd087b8e3c3e419c201f55913e
Artur Morelle Nista <arturnista@gmail.com>
2020-06-09 00:59:36 -0300

    Fixing sounds

---
+++ Assets/Boss/Prefabs/TakeScreeshot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeScreeshot : MonoBehaviour
+{
+#if UNITY_EDITOR
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(ScreenshotCoroutine());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            Screeshot();
+        }
+    }
+
+    IEnumerator ScreenshotCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+            Screeshot();
+        }
+    }
+
+    void Screeshot()
+    {
+        ScreenCapture.CaptureScreenshot(string.Format("Build/Screenshots/SS_{0}.png", DateTime.Now.ToString("yyyyMMddHHmmss")));
+    }
+
+#endif
+}
